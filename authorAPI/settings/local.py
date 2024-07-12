34135d29503d7a9471a65da1f2dfa64b5d9d4069
Oluwatosin <kingosenitosin@gmail.com>
2024-07-12 02:40:06 -0500

    Setup the Django Project

---
+++ authorAPI/settings/local.py
@@ -0,0 +1,13 @@
+from .base import env 
+from .base import * # noqa 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+  'DJANGO_SECRET_KEY', 
+  default = 'sNiIaF71HIdsWdlOPOTc2mfS8rExMrpbY4C3esmao2hdRqPm1RA',
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
---
+++ authorAPI/settings/production.py
@@ -0,0 +1,10 @@
+from .base import * # noqa
+from .base import env 
+
+
+ADMINS = [("Oluwatosin Oseni", "giovehenderson@gmail.com")]
+
+
+# TODO  ADD DOMAIN NAME OF THE PROD SERVER 
+
+CSRF_TRUSTED_ORIGINS = [""]
