7c2c0e9ea6cd16911138b3ce142cf5bf0b56f782
Oluwatosin <kingosenitosin@gmail.com>
2024-09-02 15:51:12 -0500

    added custom user and setup redocs for the api documentation

--- authorAPI/authorAPI/urls.py
+++ authorAPI/authorAPI/urls.py
@@ -1,24 +1,30 @@
-"""
-URL configuration for authorAPI project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Campus Voice",
+        default_version="v0.1",
+        description="Engine behind the Campus Voice",
+        contact=openapi.Contact(email="kingosenitosin@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
-    # path('api/', include('core_apps.profiles.urls')),
 ]
+
+admin.site.site_header = "Campus Voice API Admin"
+
+admin.site.site_title = "Campus Voice API Admin Portal"
+
+admin.site.index_title = "Welcome to Campus Voice API Portal"
